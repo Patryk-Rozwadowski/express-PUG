ccd3067521ca95a1183c0534662c163aa5cd5ad4
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-07-25 21:57:57 +0200

    Fixed authentication script

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '188312637774-ehubh91thma8cf39o5gdlnimpompgc6q.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '3Op4w77crESVx6E4BtSq1u5F',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
